2db3019e60358bb231f3e6119f7e2e8a135ccb02
JeremiahID <Jeremiahefe.je@gmail.com>
2024-10-22 17:04:08 +0100

    reset

--- Js/index.js
+++ Js/index.js
@@ -1,55 +1,55 @@
 /*this is the js for the hambuger drop down*/
-const menu = document.querySelector('#menu');
-const Hamburger = document.querySelector('#hamburger');
-const Cart = document.querySelector('#cart');
-const ShopBags = document.querySelector('#shopBag');
-const Close_cart = document.querySelector('#close_cart');
-const SectionOne = document.querySelector('.sectionOne');
-const SectionTwo = document.querySelector('.sectionTwo');
-const SectionThree = document.querySelector('.sectionThree');
-const SectionFour = document.querySelector('.sectionFour');
-const Header = document.querySelector('#header');
+// const menu = document.querySelector('#menu');
+// const Hamburger = document.querySelector('#hamburger');
+// const Cart = document.querySelector('#cart');
+// const ShopBags = document.querySelector('#shopBag');
+// const Close_cart = document.querySelector('#close_cart');
+// const SectionOne = document.querySelector('.sectionOne');
+// const SectionTwo = document.querySelector('.sectionTwo');
+// const SectionThree = document.querySelector('.sectionThree');
+// const SectionFour = document.querySelector('.sectionFour');
+// const Header = document.querySelector('#header');
 
 
-Cart.addEventListener('click', () =>{
-    if(ShopBags.style.display === "none"){
-        ShopBags.style.display = "block";
-    } else{
-        ShopBags.style.display = "none";
-    }
-})
+// Cart.addEventListener('click', () =>{
+//     if(ShopBags.style.display === "none"){
+//         ShopBags.style.display = "block";
+//     } else{
+//         ShopBags.style.display = "none";
+//     }
+// })
 
-Close_cart.addEventListener('click', () =>{
-    if(ShopBags.style.display === "block"){
-        ShopBags.style.display = "none";
-    } else {
-        ShopBags.style.display = "block";
-    }
-})
+// Close_cart.addEventListener('click', () =>{
+//     if(ShopBags.style.display === "block"){
+//         ShopBags.style.display = "none";
+//     } else {
+//         ShopBags.style.display = "block";
+//     }
+// })
 
-menu.addEventListener('click' , () =>{
-    if(Hamburger.style.display === "none"){
-        Hamburger.style.display = "block";
-        SectionOne.classList.add('hide');
-        SectionTwo.classList.add('hide');
-        SectionThree.classList.add('hide');
-        SectionFour.classList.add('hide');
-        Header.classList.add('menuHeader');
-        Header.style.background = "transparent";
-        Header.style.position = "absolute"
+// menu.addEventListener('click' , () =>{
+//     if(Hamburger.style.display === "none"){
+//         Hamburger.style.display = "block";
+//         SectionOne.classList.add('hide');
+//         SectionTwo.classList.add('hide');
+//         SectionThree.classList.add('hide');
+//         SectionFour.classList.add('hide');
+//         Header.classList.add('menuHeader');
+//         Header.style.background = "transparent";
+//         Header.style.position = "absolute"
 
         
-    } else {
-        Hamburger.style.display = "none";
-        SectionOne.classList.remove('hide');
-        SectionTwo.classList.remove('hide');
-        SectionThree.classList.remove('hide');
-        SectionFour.classList.remove('hide');
-        Header.classList.remove('menuHeader');
-        Header.style.position = "sticky";
-        Header.style.background = " #B6B6B6"
-    }
-})
+//     } else {
+//         Hamburger.style.display = "none";
+//         SectionOne.classList.remove('hide');
+//         SectionTwo.classList.remove('hide');
+//         SectionThree.classList.remove('hide');
+//         SectionFour.classList.remove('hide');
+//         Header.classList.remove('menuHeader');
+//         Header.style.position = "sticky";
+//         Header.style.background = " #B6B6B6"
+//     }
+// })
 
 
 
@@ -78,6 +78,18 @@ menu.addEventListener('click' , () =>{
 //     Cart.classList.toggle("sectionOneShow")
 // })
 
+
+// script for background
+const images = ['/Images/hero_cover.jpg', '/Images/hero-cover-two.jpg'];
+let index = 0;
+function changeBackground(){
+    index = (index + 1) % images.length;
+    document.body.style.backgroundImage = `url(${images[index]})`;
+    document.body.style.backgroundSize = 'cover';
+   
+}
+
+setInterval(changeBackground, 5000);
 
 
 
