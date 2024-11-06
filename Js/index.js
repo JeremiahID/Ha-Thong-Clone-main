e750683414a09ce75169d1b543abaf318c797080
JeremiahID <Jeremiahefe.je@gmail.com>
2024-11-06 17:12:10 +0100

    favorite svg successfully toggled

--- Js/index.js
+++ Js/index.js
@@ -132,24 +132,10 @@ Svg.addEventListener('click', ()=>{
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
             </svg>`
         
-   // CONDTIONAL STATEMENT TO TOGGLE THE STATE OF THE FLAG
-    if(Svg.innerHTML = isSolid){
-        Svg.innerHTML =  outline;
-        console.log('O');
-    } else {
-        Svg.innerHTML = solid;
-        console.log('S');
-    }
+   // CONDTIONAL STATEMENT TO TOGGLE THE STATE OF THE FLAG USING TENEARY OPERATOR!
+    Svg.innerHTML = isSolid ? outline : solid;    
 
-    isSolid = !isSolid; // SEETING THE FLAG TO TRUE / FALSE DEPENDING ON THE CURRENT STATE
-
-
-
-
-
-
-
-    
+    isSolid = !isSolid; // SEETING THE FLAG TO TRUE / FALSE DEPENDING ON THE CURRENT STATE.
 
 });
 
