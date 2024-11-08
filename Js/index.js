b9c266e08b3baa9587f900e89aa87edce49bf601
JeremiahID <Jeremiahefe.je@gmail.com>
2024-11-08 17:13:39 +0100

    fav new js chnage

--- Js/index.js
+++ Js/index.js
@@ -122,48 +122,33 @@ Subscribe.addEventListener('click', ()=>{
 var solid , outline; // declaring variable
 let isSolid = false; // settint the flag to false
 
+//SEETING THE CONTENT OF THE INNER HTML
+solid =  `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+<path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+</svg>`;
+outline = `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+</svg>`
+
+//GETING ALL ELEMENTS ASSOCIATED WITH THE FAV SVG's
 var svgs = document.getElementsByClassName('svg');
 
-for(var i = 0; i < svgs.length; i++){
+//LOOPING THROUGH EACH SVG TO PASS THE CONDITIONAL FUNCTION 
+for(let i = 0; i < svgs.length; i++){
     svgs[i].addEventListener('click', ()=>{
-        console.log('test')
-        console.log(svgs[0])
-
-        //SEETING THE CONTENT OF THE INNER HTML
-        solid =  `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-        <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-        </svg>`;
-        outline = `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-            </svg>`
-        
-        // CONDTIONAL STATEMENT TO TOGGLE THE STATE OF THE FLAG USING TENEARY OPERATOR!
-        svgs.innerHTML = isSolid ? outline : solid;    
-
-        // SEETING THE FLAG TO TRUE / FALSE DEPENDING ON THE CURRENT STATE.
-        isSolid = !isSolid;
-
+        Favorite(svgs[i])
     });
 }
 
-
-// function Favorite(){
-//     console.log('working');
-
-//     //SEETING THE CONTENT OF THE INNER HTML
-//     solid =  `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-//     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-//     </svg>`;
-//     outline = `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-//         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-//         </svg>`
+//FUNCTION TO CONDITIONALLY CHECK THROGH EACH SVG
+function Favorite(svgElement){
     
-//     // CONDTIONAL STATEMENT TO TOGGLE THE STATE OF THE FLAG USING TENEARY OPERATOR!
-//     svgs.innerHTML = isSolid ? outline : solid;    
+    // CONDTIONAL STATEMENT TO TOGGLE THE STATE OF THE FLAG USING TENEARY OPERATOR!
+    svgElement.innerHTML = isSolid ? outline : solid;    
 
-//     // SEETING THE FLAG TO TRUE / FALSE DEPENDING ON THE CURRENT STATE.
-//     isSolid = !isSolid;
-// }
+    // SEETING THE FLAG TO TRUE / FALSE DEPENDING ON THE CURRENT STATE.
+    isSolid = !isSolid;
+}
 
 
 
