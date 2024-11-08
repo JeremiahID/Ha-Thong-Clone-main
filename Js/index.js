11dc85b67d2e0cf300d81d08a35949e246746a18
JeremiahID <Jeremiahefe.je@gmail.com>
2024-11-08 16:25:59 +0100

    fav new js chnage

--- Js/index.js
+++ Js/index.js
@@ -127,6 +127,7 @@ var svgs = document.getElementsByClassName('svg');
 for(var i = 0; i < svgs.length; i++){
     svgs[i].addEventListener('click', ()=>{
         console.log('test')
+        console.log(svgs[0])
 
         //SEETING THE CONTENT OF THE INNER HTML
         solid =  `<svg class="favorite" id="favorite-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
