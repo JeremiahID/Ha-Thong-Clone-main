4842749b713e08822d1758d867780e652c7eeef5
JeremiahID <jeremiahefe.je@gmail.com>
2024-11-15 13:26:17 +0100

    interacticve cart added to the best seller dress

--- Js/index.js
+++ Js/index.js
@@ -150,13 +150,10 @@ function Favorite(svgElement){
 }
 
 
-
-                //  ADD AND SUB IN QUANTITY
-
-//selecting all cart items
-const cartItems = document.querySelectorAll('.cart-items'); 
-const checkOutPrice = document.getElementById('check-out-price');
-const checkOutSubTotal = document.getElementById('subTotal')
+//  ADD AND SUB IN QUANTITY
+const cartItems = document.querySelectorAll('.cart-items'); //selecting all cart items
+const checkOutPrice = document.getElementById('check-out-price'); //selecting all cart items
+const checkOutSubTotal = document.getElementById('subTotal'); //selecting all cart items
 
 //Looping throught each cart  items
 cartItems.forEach(item => {
@@ -235,7 +232,7 @@ calculateSubTotal();
 var cartSolid, cartOutline, isCartSolid;// variable declaration
 isCartSolid = false; // setting the state of the toggling to false
 
-var cartAddIcon = document.getElementsByClassName('cart-add-icon');// getting the container for the each cart items icon
+var cartAddIcon = document.querySelectorAll('.cart-add-icon');// getting the container for the each cart items icon
 
 cartSolid = `<svg class="icon-cart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
@@ -248,17 +245,24 @@ cartOutline = `<svg class="icon-cart" xmlns="http://www.w3.org/2000/svg" fill="n
 // looping through each conatainer
 for(let i = 0; i < cartAddIcon.length; i++){
     cartAddIcon[i].addEventListener('click', ()=>{
-        cartItemIcon(cartAddIcon[i])
+        cartItemIcon(cartAddIcon[i]);
     });
-}
+};
+
+// cartAddIcon.forEach(addicon =>{
+//     const me = addicon.querySelector('#icon-cart-test');
+//     me.addEventListener('click' , ()=>{
+//         cartItemIcon(me);
+//     })
+// });
 
 
 //FUNCTION TO CONDITIONALLY CHECK THROGH EACH SVG
 function cartItemIcon(cartElement){
     
     // CONDTIONAL STATEMENT TO TOGGLE THE STATE OF THE FLAG USING TENEARY OPERATOR!
-        cartElement.innerHTML = isCartSolid ? cartSolid : cartOutline;
+        cartElement.innerHTML = isCartSolid ? cartOutline : cartSolid;
 
     // SETTING THE FLAG TO TRUE / FALSE DEPENDING ON THE CURRENT STATE.
-    isCartSolid = !isCartSolid
+    isCartSolid = !isCartSolid;
 }
